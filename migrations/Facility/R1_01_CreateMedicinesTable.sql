a8c51101eebb8c13cdaa969da681be1d43813a10
Fernando Pineda <fernando.pineda2199@gmail.com>
2025-05-16 12:18:20 -0600

    fix: removes dynamo reserverd keywords

--- migrations/Facility/R1_01_CreateMedicinesTable.sql
+++ migrations/Facility/R1_01_CreateMedicinesTable.sql
@@ -1,3 +1,3 @@
--- Rollback script for medicine table
-DROP TABLE IF EXISTS medicine CASCADE;
-DROP INDEX IF EXISTS idx_medicine_name;
+-- Rollback script for medicines table
+DROP TABLE IF EXISTS medicines CASCADE;
+DROP INDEX IF EXISTS idx_medicines_name;
