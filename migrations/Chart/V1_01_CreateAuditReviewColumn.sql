fb2d3896c80b482a8bb4497c734a34462bdc23a4
Jorge Zapata <>
2025-05-19 10:53:09 -0600

    chore: chart section-subsection audit review property

---
+++ migrations/Chart/V1_01_CreateAuditReviewColumn.sql
@@ -0,0 +1,22 @@
+-- # Created At: 13-05-2025
+-- # Description: The following columns will be used for storing the review value of a section and subsection
+-- # Related to: EN-2691
+
+DO $$
+BEGIN
+    IF EXISTS (SELECT FROM pg_tables WHERE tablename = 'ChartSections') THEN
+        ALTER TABLE public."ChartSections" ADD COLUMN IF NOT EXISTS "AuditReview" bool NOT NULL DEFAULT false;
+    END IF;
+
+    IF EXISTS (SELECT FROM pg_tables WHERE tablename = 'ChartEncounterSections') THEN
+        ALTER TABLE public."ChartEncounterSections" ADD COLUMN IF NOT EXISTS "AuditReview" bool NOT NULL DEFAULT false;
+    END IF;
+
+    IF EXISTS (SELECT FROM pg_tables WHERE tablename = 'ChartSubSections') THEN
+        ALTER TABLE public."ChartSubSections" ADD COLUMN IF NOT EXISTS "AuditReview" bool NOT NULL DEFAULT false;
+    END IF;
+
+    IF EXISTS (SELECT FROM pg_tables WHERE tablename = 'ChartEncounterSubSections') THEN
+        ALTER TABLE public."ChartEncounterSubSections" ADD COLUMN IF NOT EXISTS "AuditReview" bool NOT NULL DEFAULT false;
+    END IF;
+END $$;
