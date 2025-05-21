79028ac2e294f55d6fed18510245811c7a29b7c6
Victor Pulido <ve.pulido@hotmail.com>
2025-05-20 21:27:51 -0600

    feat: observation staff new columns

---
+++ migrations/Patient/R1_01_ObservationStaff.sql
@@ -0,0 +1,4 @@
+ALTER TABLE public."Encounters" DROP COLUMN "ObservationSupervisorId";
+ALTER TABLE public."Encounters" DROP COLUMN "ObservationChargeNurseId";
+ALTER TABLE public."Encounters" DROP COLUMN "ObservationScribeId";
+ALTER TABLE public."Encounters" DROP COLUMN "ObservationClinicalSupportId";
---
+++ migrations/Patient/V1_01_ObservationStaff.sql
@@ -0,0 +1,4 @@
+ALTER TABLE public."Encounters" ADD "ObservationSupervisorId" uuid NULL;
+ALTER TABLE public."Encounters" ADD "ObservationChargeNurseId" uuid NULL;
+ALTER TABLE public."Encounters" ADD "ObservationScribeId" uuid NULL;
+ALTER TABLE public."Encounters" ADD "ObservationClinicalSupportId" uuid NULL;
